b5e2a69f46c2653258496f87f1700f11955900a5
kgryte <kgryte@gmail.com>
2016-03-05 17:39:49 -0800

    Add test fixtures, add main export, and add associated tests

---
+++ test/fixtures/opts.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function getOpts() {
+	var opts = {
+		'token': 'abcdefg'
+	};
+	return opts;
+}
+
+
+// EXPORTS //
+
+module.exports = getOpts;
---
+++ test/test.create.js
@@ -0,0 +1,132 @@
+'use strict';
+
+// MODULES //
+
+var tape = require( 'tape' );
+var proxyquire = require( 'proxyquire' );
+var noop = require( '@kgryte/noop' );
+var create = require( './../lib/create.js' );
+
+
+// FIXTURES //
+
+var getOpts = require( './fixtures/opts.js' );
+var results = require( './fixtures/results.json' );
+
+
+// TESTS //
+
+tape( 'file exports a function', function test( t ) {
+	t.equal( typeof create, 'function', 'export is a function' );
+	t.end();
+});
+
+tape( 'function throws if provided an invalid options argument', function test( t ) {
+	var values;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
+	}
+	t.end();
+
+	function badValue( value ) {
+		return function badValue() {
+			create({
+				'token': value
+			}, noop );
+		};
+	}
+});
+
+tape( 'function throws if provided a callback argument which is not a function', function test( t ) {
+	var values;
+	var opts;
+	var i;
+
+	values = [
+		'5',
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{}
+	];
+
+	opts = getOpts();
+	for ( i = 0; i < values.length; i++ ) {
+		t.throws( badValue( values[i] ), TypeError, 'throws a type error when provided ' + values[i] );
+	}
+	t.end();
+
+	function badValue( value ) {
+		return function badValue() {
+			create( opts, value );
+		};
+	}
+});
+
+tape( 'function returns an error to a provided callback if an error is encountered when creating a token', function test( t ) {
+	var create;
+	var opts;
+
+	create = proxyquire( './../lib/create.js', {
+		'./query.js': query
+	});
+
+	opts = getOpts();
+	create( opts, done );
+
+	function query( opts, clbk ) {
+		setTimeout( onTimeout, 0 );
+		function onTimeout() {
+			clbk( new Error( 'beep' ) );
+		}
+	}
+
+	function done( error ) {
+		t.ok( error instanceof Error, 'error instance' );
+		t.equal( error.message, 'beep' );
+		t.end();
+	}
+});
+
+tape( 'function returns response data to a provided callback', function test( t ) {
+	var expected;
+	var create;
+	var opts;
+
+	create = proxyquire( './../lib/create.js', {
+		'./query.js': query
+	});
+
+	expected = results;
+
+	opts = getOpts();
+	create( opts, done );
+
+	function query( opts, clbk ) {
+		setTimeout( onTimeout, 0 );
+		function onTimeout() {
+			clbk( null, results );
+		}
+	}
+
+	function done( error, data ) {
+		t.deepEqual( data, expected, 'deep equal' );
+		t.end();
+	}
+});

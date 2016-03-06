daa9635d3cd8bfcef6ce71c63f411fc0ff26541b
kgryte <kgryte@gmail.com>
2016-03-05 17:02:52 -0800

    Add example

---
+++ examples/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var create = require( './../lib' );
+
+var opts = {
+	'token': '<your_token_goes_here>'
+};
+
+create( opts, clbk );
+
+function clbk( error, results ) {
+	if ( error ) {
+		throw new Error( error.message );
+	}
+	console.dir( results );
+}

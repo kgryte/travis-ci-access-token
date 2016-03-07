023781af96a77b18bf742dae21a5502d53fb984d
kgryte <kgryte@gmail.com>
2016-03-07 05:25:26 -0800

    Add hostname option support

--- test/test.validate.js
+++ test/test.validate.js
@@ -93,6 +93,32 @@ tape( 'if provided a `useragent` option which is not a primitive string, the fun
 	t.end();
 });
 
+tape( 'if provided a `hostname` option which is not a primitive string, the function returns a type error', function test( t ) {
+	var values;
+	var err;
+	var i;
+
+	values = [
+		5,
+		NaN,
+		null,
+		undefined,
+		true,
+		[],
+		{},
+		function(){}
+	];
+
+	for ( i = 0; i < values.length; i++ ) {
+		err = validate( {}, {
+			'token': 'abcdefg',
+			'hostname': values[i]
+		});
+		t.ok( err instanceof TypeError, 'returns type error when provided ' + values[i] );
+	}
+	t.end();
+});
+
 tape( 'the function returns `null` if all options are valid', function test( t ) {
 	var options;
 	var opts;
@@ -100,7 +126,8 @@ tape( 'the function returns `null` if all options are valid', function test( t )
 
 	options = {
 		'token': 'abcdefg',
-		'useragent': 'beeper-booper'
+		'useragent': 'beeper-booper',
+		'hostname': 'api.travis-ci.com'
 	};
 
 	opts = {};
